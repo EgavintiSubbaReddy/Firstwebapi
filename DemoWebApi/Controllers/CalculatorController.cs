293a9c1c2457f40738a21a33038ded6f260ca2de
Egavinti Subbareddy <egavinti.s@gavstech.com>
2023-09-21 12:14:47 +0530

    firstcommit

---
+++ DemoWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        //api/calculator/Add?x=10 & y=20
+        [HttpGet("/AddCalculator")]
+        public int Add(int x, int y)
+        { return x + y; }
+
+        [HttpGet("/Sum")]
+        public int Sum(int x, int y)
+        { return x + y + 1000; }
+
+        //api/calculator/Subtract?x=30 & y=20
+        [HttpPost]
+        public int Subtract(int x, int y)
+        { return x - y; }
+        
+        //api/calculator/multiply?x=10 & y=20
+        [HttpPut]
+        public int Multiply(int x, int y)
+        { return x * y; }
+        //api/calculator/Divide?x=40 & y=20
+        [HttpDelete]
+        public int Divide(int x, int y)
+        { return x / y; }
+        
+    }
+}
---
+++ DemoWebApi/Controllers/DemoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        //api/calculator/Add?x=10 & y=20
+        [HttpGet("/DemoAdd")]
+        public int DemoAdd(int x, int y)
+        { return x + y; }
+    }
+}
