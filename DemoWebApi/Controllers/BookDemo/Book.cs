ae9e931360edba93d037a3bbababd37c1a9f7297
Egavinti Subbareddy <egavinti.s@gavstech.com>
2023-09-21 14:52:26 +0530

    firstcommit

---
+++ DemoWebApi/Controllers/BookDemo/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoWebApi.Controllers.BookDemo
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+
+
+
+        public string AuthorName { get; set; }
+
+    }
+}
